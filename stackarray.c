ecfb4849061190e4c110fc815be21baa687d134f
Aneeta2002 <aneetasebastian0405@gmail.com>
2025-11-15 22:07:43 +0530

    Update stackarray.c

--- stackarray.c
+++ stackarray.c
@@ -1,11 +1,78 @@
 #include<stdio.h>
-#define Max 100
-int stack[Max]top=-1;
+#define MAX 3
+int stack [MAX],top=-1,value;
 void push()
-if top==100-1;
 {
-printf("overflow");
+    if(top==MAX-1)
+    {
+        printf("stack overflow");
+    }
+    else
+    {
+        printf("enter the value to push:");
+        scanf("%d",&value);
+        stack[++top]=value;
+        printf("%d pushed on to the stack\n",value);
+    }
 }
-else
+void pop()
 {
-
+    if (top==-1)
+    {
+        printf("stack underflow");
+    }
+    else
+    {
+        value=stack[top];
+        top=top-1;
+        printf("%d popped from the stack",value);
+    }
+}
+void display()
+{
+    if(top==-1)
+    {
+        printf("stack empty");
+    }
+    else
+    {
+        printf("stack elements:");
+        for(int i=top;i>=0;i--)
+        {
+            printf("%d\t",stack[i]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int choice;
+    while(1)
+    {
+        printf("\nMENU");
+        printf("\n1.PUSH");
+        printf("\n2.POP");
+        printf("\n3.DISPLAY");
+        printf("\n4.EXIT");
+        printf("\nEnter your choice=");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            push();
+            break;
+            case 2:
+            pop();
+            break;
+            case 3:
+            display();
+            break;
+            case 4:
+            printf(".......exiting.....\n");
+            return 0;
+            default:
+            printf("invalid choice");
+        }
+    }
+    return 0;
+}
